ad7f99f1df8faec20fb3da48c55fbbbef5e9f97d
GlebOmelchenko <81290562+GlebOmelchenko@users.noreply.github.com>
2022-08-28 20:10:24 +0300

    lessons1 var2

--- src/main/java/lessons1/pyzurkom.java
+++ src/main/java/lessons1/pyzurkom.java
@@ -4,20 +4,39 @@ import java.util.Arrays;
 
 public class pyzurkom {
     public static void main(String[] args) {
-        int [] array = new int[] {15 , 62 , 14 , 16 , 21 , 6 , 9 , 51, 63 , 46 , 74};
-        sort(array);
+        int [] array = new int[] {15 , 62 , 14 , 16 , 21 , 6 , 9 , 51, 63 , 46 , 57};
+        System.out.println(Arrays.toString(array));
+        System.out.println("");
+//        sorttwofor(array);
+        sortwhile(array);
     }
 
-    public static void sort(int [] array) {
-        for(int i=0 ; i< array.length; i++){
-            for(int j =0 ; j<array.length; j++) {
+    public static void sorttwofor(int [] array) {
+        for(int i = 0 ; i< array.length; i++){
+            for(int j = 0 ; j<array.length-1; j++) {
                 if(array[i] < array[j]){
                     int temp = array[i];
                     array[i] = array[j];
                     array[j] = temp;
                 }
             }
+            System.out.println(Arrays.toString(array));
         }
-        System.out.println(Arrays.toString(array));
+    }
+
+    public static void sortwhile(int [] array){
+        boolean isSorted = false;
+        while(!isSorted){
+            isSorted=true;
+            for(int i = 1; i<array.length; i++){
+                if(array[i] < array[i-1]){
+                    isSorted=false;
+                    int temp = array[i];
+                    array[i] = array[i-1];
+                    array[i-1] = temp;
+                }
+            }
+            System.out.println(Arrays.toString(array));
+        }
     }
 }
